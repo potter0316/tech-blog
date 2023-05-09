aebe49119f6486617ef044dbd112f31bdc9e48a2
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-05-08 19:23:06 -0500

    changed handlebar files blog dashboard and login

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Comment.findAll({})
-        .then(commenData => res.json(commentData))
+        .then(commentData => res.json(commentData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err)
